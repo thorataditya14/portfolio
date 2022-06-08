db75cda080b147d2595645935e016ad35772fa4a
Aditya Thorat <thorat.aditya@outlook.com>
2022-06-08 19:59:37 +0530

    complete Topbar component

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,26 +1,25 @@
-import "./menu.scss"
+import "./menu.scss";
 
-
-export default function Menu({menuOpen, setMenuOpen}) {
+export default function Menu({ menuOpen, setMenuOpen }) {
   return (
-    <div className={"menu " + (menuOpen && "active")}>
-        <ul>
-            <li onClick={() => setMenuOpen(!menuOpen)}>
-                <a href="#intro">Home</a>
-            </li>
-            <li onClick={() => setMenuOpen(!menuOpen)}>
-                <a href="#portfolio">Portfolio</a>
-            </li>
-            <li onClick={() => setMenuOpen(!menuOpen)}>
-                <a href="#works">Works</a>
-            </li>
-            <li onClick={() => setMenuOpen(!menuOpen)}>
-                <a href="#testimonials">Testimonials</a>
-            </li>
-            <li onClick={() => setMenuOpen(!menuOpen)}>
-                <a href="#contact">Contact</a>
-            </li>
-        </ul>
+    <div className={"menu "+(menuOpen && "active")}>
+      <ul>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="#intro">Home</a>
+        </li>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="#works">Works</a>
+        </li>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
