69c7bf94e39c9315324eb63902bea4f5df1dab0e
Aditya Thorat <thorat.aditya@outlook.com>
2022-06-08 17:09:42 +0530

    complete Intro component

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,46 @@
+import { useEffect, useRef } from "react"
+import { init } from "ityped"
 import "./intro.scss"
 
+
 export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(
+            textRef.current,
+            {
+                showCursor: true,
+                // backDelay: 1000,
+                // backSpeed: 500000,
+                strings: [
+                    "Developer",
+                    "Designer",
+                    "Content Creator"
+                ]
+            }
+        );
+    }, []);
+
+
     return (
         <div className="intro" id="intro">
-            Intro
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Firstname Lastname</h1>
+                    <h3>Freelance <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
         </div>
-)
+    )
 }
